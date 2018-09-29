c4ddf2109421bbe993aba33886ff55bbe9513999
GhostMajik <colin.pugh@georgebrown.ca>
2018-09-29 14:27:01 -0400

    Added platform movement behavior and platforming spawning controller and behavior.

---
+++ GBC_Jam_Excitement/Assets/_TestFolder/Colin/PlatformScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+
+    public bool isRunning;
+    private float speed = 1.0f;
+    //magic number used until screen width calculation is done
+    private float endPoint = -6.0f;
+    private Material defaultMaterial;
+
+	// Use this for initialization
+	void Start () {
+        isRunning = true;
+        defaultMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x <= endPoint) {
+            isRunning = false;
+        }
+        if (!isRunning) {
+            Destroy(this.gameObject, 0.3f);
+        }
+        
+	}
+    //move platforms
+    private void FixedUpdate(){
+        if (isRunning) {
+            transform.Translate(transform.right * -speed * Time.deltaTime, Space.World);   
+        }
+    }
+    //get transform can be used for sending the colors to platform position
+    public Vector3 getPostion() {
+        return transform.position;
+    }
+
+    //set and get movement speed of the platforms.
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float inSpeed) {
+        speed = inSpeed;
+    }
+
+}
---
+++ GBC_Jam_Excitement/Assets/_TestFolder/Colin/SpawnerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour {
+    private float timeBetweenSpawns = 0.0f;
+    private float gameTimer = 0.0f;
+    private bool gameIsRunning = false;
+
+    [SerializeField]
+    private GameObject platformPrefab;
+    [SerializeField]
+    private float spawnTimer = 5.0f;
+    [SerializeField]
+    private Transform spawnPoint;
+
+	// For initialization
+	void Start () {
+      
+	}
+	
+	// Update
+	void Update () {
+        gameTimer += Time.deltaTime;
+        if (gameTimer >= 1.0f) {
+            gameIsRunning = true;
+        }
+        if (gameIsRunning){
+            timeBetweenSpawns += Time.deltaTime;
+            if (timeBetweenSpawns >= spawnTimer){ 
+                Instantiate(platformPrefab, spawnPoint);
+                timeBetweenSpawns = 0.0f;
+            }
+        }
+	}
+}
